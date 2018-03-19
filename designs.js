448d820992aacca60662fd3816138e992f3726ec
Emily Somach <emiksoma@gmail.com>
2018-03-19 17:32:24 -0400

    removed variables in function, pass them in as params now

--- designs.js
+++ designs.js
@@ -9,17 +9,17 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height, weight) {
     //store user input color as variable
-    var color = document.getElementById('colorPicker').value;
+//    var color = document.getElementById('colorPicker').value;
     
     //change border color to user input
     //$("table").css("border", "1px solid" + color);
     
     //store user input height as variable
-    var height = document.getElementById('inputHeight').value;
+//    var height = document.getElementById('inputHeight').value;
     //store user input weight as variable
-    var weight = document.getElementById('inputWeight').value;
+//    var weight = document.getElementById('inputWeight').value;
 
     for (var h; h < height; h++) {
         var row = $('table').insertRow();
